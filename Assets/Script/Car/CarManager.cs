32702a6c77bcfd93ec356766297d2346c6473183
김동건 <126738169+zopa2161@users.noreply.github.com>
2026-08-31 21:58:50 +0900

    휠콜라이더 리팩토링
    휠콜라이더 사용

--- Assets/Script/Car/CarManager.cs
+++ Assets/Script/Car/CarManager.cs
@@ -4,56 +4,52 @@ using UnityEngine.Rendering;
 
 public class CarManager : MonoBehaviour
 {
+    public WheelCollider FLWheel;
+    public WheelCollider FRWheel;
+    public WheelCollider RLWheel;
+    public WheelCollider RRWheel;
+
+
 
     public Rigidbody RB;
 
-    //증강에 영향받는 상태값.
-    public float CarMaxSpeed;
-    public float CarAccelerationLevel;
-    public float CarBrakeLevel;
-    public float SteeringLevel;
-    public float Mass;
+    private CarStat stat;
+
+ 
 
     //단순 상태값.
     public float Speed => RB.linearVelocity.magnitude * 3.6f;
 
+
+
+
     private void Start()
     {
         RB = GetComponent<Rigidbody>();
+        stat = GetComponent<CarStat>();
+        
     }
 
     public void Accelerate(int direction)
     {
-        if(RB != null) 
-        {
-            if (direction == 1)
-            {
-                RB.AddForce(transform.forward * CarAccelerationLevel);
-            }
-            else if (direction == -1) 
-            {
-                RB.AddForce(transform.forward * -CarAccelerationLevel);
-            }
-        }
+        RLWheel.motorTorque = direction * stat.Acceleration;
+        RRWheel.motorTorque = direction * stat.Acceleration;
+
     }
     public void Steer(int steer)
     {
-        if (steer == -1)
-        {
-            RB.AddForce(transform.right * -CarAccelerationLevel);
-            RB.AddRelativeTorque(Vector3.up * 1);
 
-        }
-        else if (steer == 1)
-        {
-            RB.AddForce(transform.right * CarAccelerationLevel);
-            RB.AddRelativeTorque(Vector3.up * -1);
-        }
     }
  
     public void Brake()
     {
-        RB.linearVelocity = RB.linearVelocity / CarBrakeLevel;
+        RLWheel.motorTorque = 0f;
+        RRWheel.motorTorque = 0f;
+
+        FLWheel.brakeTorque = stat.Braking;
+        FRWheel.brakeTorque = stat.Braking;
+        RLWheel.brakeTorque = stat.Braking;
+        RLWheel.brakeTorque = stat.Braking;
     }
 
 
