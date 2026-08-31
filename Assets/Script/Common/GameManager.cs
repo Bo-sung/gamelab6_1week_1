4e46eb7c74e574e21fff3c12c1e6e6827255db37
seo-bosung <ssbss0329@gmail.com>
2026-08-31 22:05:20 +0900

    fix : 컨플릭트 해결

--- Assets/Script/Common/GameManager.cs
+++ Assets/Script/Common/GameManager.cs
@@ -7,32 +7,84 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    AugmentManager agManager;
+    AugmentManger agManager;
     [SerializeField]
     CarStat carStat;
+    [SerializeField]
+    TrackManager trackManager;
 
     private void Awake()
     {
         if (agManager == null)
         {
-            agManager = GetComponent<AugmentManager>();
+            agManager = GetComponent<AugmentManger>();
+        }
+        if (trackManager == null)
+        {
+            trackManager = GetComponent<TrackManager>();
         }
         agManager.OnSelectAugment += HandleAugmentSelected;
+        trackManager.OnCarTrackFinish += HandleCarTrackFinish;
     }
 
-    private void HandleAugmentSelected(int obj)
+    private void HandleAugmentSelected(Augment data)
     {
-        throw new NotImplementedException();
+        switch (data.type)
+        {
+            case AugmentType.SpeedUp:
+                carStat.ApplySpeed(data.value);
+                Debug.Log("SpeedUp " + data.value);
+                break;
+            case AugmentType.AccelerationUp:
+                carStat.ApplyAcceleration(data.value);
+                Debug.Log("AccelerationUp " + data.value);
+                break;
+            case AugmentType.BrakeUp:
+                carStat.ApplyBraking(data.value);
+                Debug.Log("BrakeUp " + data.value);
+                break;
+            case AugmentType.CorneringUp:
+                carStat.ApplyCornering(data.value);
+                Debug.Log("CorneringUp " + data.value);
+                break;
+            case AugmentType.WeightUp:
+                carStat.ApplyWeight(data.value);
+                Debug.Log("WeightUp " + data.value);
+                break;
+            case AugmentType.LoseWheel:
+                Debug.Log("LoseWheel");
+                break;
+            case AugmentType.ThrowBreak:
+                Debug.Log("ThrowBreak");
+                break;
+            case AugmentType.SpringBumper:
+                Debug.Log("SpringBumper");
+                break;
+        }
+    }
+
+    private void HandleCarTrackFinish()
+    {
+        agManager.ActiveScreen();
     }
 
     public void StartGame()
     {
-        // Implement game start logic here
         Console.WriteLine("Game Started");
     }
     public void EndGame()
     {
-        // Implement game end logic here
         Console.WriteLine("Game Ended");
     }
+}
+public class TrackManager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> trackPool = new List<GameObject>();
+
+    public System.Action OnCarTrackFinish;
+
+
+
+
 }
