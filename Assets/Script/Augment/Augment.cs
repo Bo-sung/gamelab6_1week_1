cb5e7df23c6141831c8a4d3df41afe451223f815
황병부 <hbb8049@gmail.com>
2026-08-31 20:27:24 +0900

    feat: 증강 시스템 구현

---
+++ Assets/Script/Augment/Augment.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public enum AugmentType
+{
+    SpeedUp,
+    LoseWheel,
+    ThrowBreak,
+    SpringBumper
+}
+
+
+[CreateAssetMenu(fileName = "Augment", menuName = "Scriptable Objects/Augment")]
+public class Augment : ScriptableObject
+{
+    public string augmentName;
+    public string description;
+    //TODO : 증강 최대 한계를 게임매니저에서 관리할 것인지 증강매니저에서 관리할 것인지 등  
+    public int limit;
+    public AugmentType type;
+}   
+
---
+++ Assets/Script/Augment/AugmentManger.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using NUnit.Framework;
+
+public class AugmentManger : MonoBehaviour
+{
+    public bool isActive = false;
+    // public GameObject gameManager; 게임매니저 참조?
+    public GameObject augmentPanel;
+    public GameObject[] augmentButtons;
+    public List<Augment> augmentList = new List<Augment>();
+    public List<Augment> selectedAugments = new List<Augment>();
+
+    [ContextMenu("강제 증강 출현")]
+    public void ActiveScreen()
+    {
+
+        Time.timeScale = 0.0f;
+        selectedAugments = GetRandomAugment(augmentList, 3);
+        for (int i = 0; i < augmentButtons.Length; i++)
+        {
+            augmentButtons[i].SetActive(true);
+            augmentButtons[i].transform.Find("NameText").GetComponent<TextMeshProUGUI>().text = selectedAugments[i].augmentName;
+            augmentButtons[i].transform.Find("DescriptionText").GetComponent<TextMeshProUGUI>().text = selectedAugments[i].description;
+        }
+        augmentPanel.SetActive(true);
+    }
+
+    public void DisableScreen()
+    {
+        Time.timeScale = 1.0f;
+        for (int i = 0; i < augmentButtons.Length; i++)
+        {
+            augmentButtons[i].SetActive(false);
+        }
+        augmentPanel.SetActive(false);
+    }
+
+
+    public List<Augment> GetRandomAugment(List<Augment> augmentList, int count)
+    {
+        List<Augment> randomAugments = new List<Augment>();
+        List<int> selectedIndex = new List<int>();
+        while (randomAugments.Count < count)
+        {
+            int randomIndex = Random.Range(0, augmentList.Count);
+            if (!selectedIndex.Contains(randomIndex))
+            {
+                selectedIndex.Add(randomIndex);
+                randomAugments.Add(augmentList[randomIndex]);
+            }
+        }
+        return randomAugments;
+    }
+
+    public void SelectAgument(int index)
+    {
+        Augment data = selectedAugments[index];
+        switch(data.type)
+        {
+            case AugmentType.SpeedUp:
+                Debug.Log("SpeedUp");
+                break;
+            case AugmentType.LoseWheel:
+                Debug.Log("LoseWheel");
+                break;
+            case AugmentType.ThrowBreak:
+                Debug.Log("ThrowBreak");
+                break;
+            case AugmentType.SpringBumper:
+                Debug.Log("SpringBumper");
+                break;
+        }
+        DisableScreen();
+    }
+}
