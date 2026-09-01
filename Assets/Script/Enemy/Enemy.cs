72f2b364d567b790c03c93b084de2a78a846a6bc
백현진 <jinbaik2007@gmail.com>
2026-09-01 22:00:43 +0900

    feat: 적 접근 거리 제한
    .

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -5,7 +5,10 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private Transform player;
 
+    // 접근 속도
     public float speed = 0.1f;
+    // 최대 접근 거리
+    public float maxDistance = 2;
 
     private void Update()
     {
@@ -14,6 +17,7 @@ public class Enemy : MonoBehaviour
 
     private void Movement()
     {
+        if(Vector3.Distance(this.transform.position, player.position) >= maxDistance)
         this.transform.position = Vector3.MoveTowards(this.transform.position, player.position, speed);
     }
 
