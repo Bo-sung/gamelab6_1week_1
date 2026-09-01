4f6763c02cd2142092e12a50e7292c3d953b67fd
황병부 <hbb8049@gmail.com>
2026-09-01 21:45:26 +0900

    feat: 게임매니저 및 플레이어 추가

---
+++ Assets/Script/GameFlowManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameFlowManager : MonoBehaviour
+{
+    public Player player;
+    public GameOverUI gameOverUI;
+
+    private void Awake()
+    {
+        player.OnPlayerDead += GameOver;
+    }
+
+    private void GameOver()
+    {
+        player.gameObject.SetActive(false);
+        Debug.Log("Game Over");
+        gameOverUI.ShowGameOverScreen();
+    }
+}
