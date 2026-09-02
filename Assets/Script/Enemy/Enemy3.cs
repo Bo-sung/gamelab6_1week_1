fefaab5dd4f86c5b8bd4cbf96206a7235c1fef8d
황병부 <hbb8049@gmail.com>
2026-09-02 15:43:24 +0900

    feat: 에너미3 추가

---
+++ Assets/Script/Enemy/Enemy3.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Enemy3 : EnemyBase
+{
+    public float dashInterval = 3f;
+    private float lastDashTime = 0f;
+    public float minDashDistance = 0.5f;
+    public float maxDashDistance = 2.0f;
+    public float range = 1.0f;
+    protected override void Movement()
+    {
+        if (Time.time - lastDashTime >= dashInterval)
+        {
+            Dash();
+            lastDashTime = Time.time;
+        }
+    }
+
+    protected override void OnHit(Collider other)
+    {
+    }
+
+    protected override void Init()
+    {
+        lastDashTime = Time.time;
+    }
+
+    protected override void OnDamage()
+    {
+        ApplyScore(40);
+        this.gameObject.SetActive(false);
+    }
+
+    private void Dash()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        float dashDistance = Random.Range(minDashDistance, maxDashDistance);
+        transform.position += direction * dashDistance;
+        transform.position += transform.right * Random.Range(-range, range);
+        if (Vector3.Distance(transform.position, target.position) < maxDistance)
+        {
+            transform.position = target.position - (target.position - transform.position).normalized * maxDistance;
+        }   
+    }
+}
